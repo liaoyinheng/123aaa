4f0e1aa73d7c3b29857c426f0a0ff58c0dcccc70
liao <1351109471@qq.com>
2019-06-21 22:36:29 +0800

    我的第一个git库

---
+++ src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="wel">
+    欢迎你使用电商后台管理系统
+    <!-- <div style="background:url(../../../assets/logo.png)"></div> -->
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+.wel{
+    color:#000;
+}
+</style>
---
+++ src/views/goods/goods.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="list">
+        goods
+        <!-- 嵌套路由的展示区域 -->
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="less" scoped>
+
+</style>
